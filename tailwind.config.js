4398cdb647d773485314618fc9d0a2754989bf5f
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-14 16:31:35 -0800

    mobile template

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'neon':'#C7FFC2'
+      }
+    },
+  },
+  plugins: [],
+}
